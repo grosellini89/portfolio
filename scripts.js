de6e6d5239d2c60776240ac13ad401ebc7c9ac01
grosellini89 <75783532+grosellini89@users.noreply.github.com>
2021-01-12 11:45:13 +0100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+let navbar = document.querySelector('#navbar')
+let li= document.querySelectorAll(".nav-link")
+console.log(li)
+
+document.addEventListener('scroll', ()=> {
+
+    if (window.pageYOffset > 100) {
+        navbar.classList.add('shadow','bg-light')
+        for(let a of li){
+        a.classList.remove('text-white')
+        a.classList.add('text-black-50')
+    }
+    } else {
+        navbar.classList.remove('shadow','bg-light')
+        for(let a of li){
+            a.classList.remove('text-black-50')
+            a.classList.add('text-white')
+        }    }
+    
+} )
+
